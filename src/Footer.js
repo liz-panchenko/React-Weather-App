ea4731f8eef427f856efad99b34efde799709423
liz-panchenko <90611427+liz-panchenko@users.noreply.github.com>
2021-11-16 11:26:32 +0000

    Footer GitHub project link updated

--- src/Footer.js
+++ src/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
         <span className="codder-name">Liz Panchenko</span>
         and is open-sourced on
         <a
-          href="https://github.com/liz-panchenko/my-weather-project"
+          href="https://github.com/liz-panchenko/React-Weather-App"
           target="_blank"
           rel="noreferrer"
         >
