9463085c9831f6bf1b1fae3f19573ecaa8933e89
liz-panchenko <90611427+liz-panchenko@users.noreply.github.com>
2021-11-26 10:58:06 +0000

    Forecast temp conversion

--- src/Forecast.js
+++ src/Forecast.js
@@ -2,22 +2,41 @@ import React from "react";
 import "./Forecast.css";
 
 export default function Forecast({ fUnits, weekday }) {
+  function forecastTemp() {
+    //   let forecastCTemp = Math.round(searchResults.main.temp);
+    let forecastCTemp = 18;
+    let forecastFTemp = Math.round((forecastCTemp * 9) / 5 + 32);
+    if (fUnits) {
+      return (
+        <span>
+          <span>{forecastFTemp}</span>
+          °F
+        </span>
+      );
+    } else {
+      return (
+        <div>
+          <span>{forecastCTemp}</span>
+          °C
+        </div>
+      );
+    }
+  }
   return (
-      <div className="Forecast col-sm-4 col-md my-2">
-        <div className="card text-center weather-card">
-          <div className="card-body">
-            <div className="card-title">{weekday}</div>
-            <img
-              src="${cardIcon(dailyForecast.weather[0].icon)}"
-              className="card-img"
-              alt="clouds"
-            />
-            <p className="card-text">
-              <span className="daily-temp card-CTemp">18</span>
-              °C
-            </p>
-          </div>
+    <div className="Forecast col-sm-4 col-md my-2">
+      <div className="card text-center weather-card">
+        <div className="card-body">
+          <div className="card-title">{weekday}</div>
+          <img
+            src="${cardIcon(dailyForecast.weather[0].icon)}"
+            className="card-img"
+            alt="clouds"
+          />
+          <p className="card-text">
+            <span className="daily-temp card-CTemp">{forecastTemp()}</span>
+          </p>
         </div>
       </div>
+    </div>
   );
 }
