78165fc3ce38ddb50ecd11f297efd6a9aeae1c0a
liz-panchenko <90611427+liz-panchenko@users.noreply.github.com>
2021-11-29 15:13:23 +0000

    Link to Portfolio page added

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,14 @@ export default function Footer() {
     <div className="Footer">
       <footer>
         This project was coded by
-        <span className="codder-name">Liz Panchenko</span>
+        <a
+          href="https://elated-snyder-278566.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Liz Panchenko{" "}
+        </a>
         and is open-sourced on{" "}
         <a
           href="https://github.com/liz-panchenko/React-Weather-App"
